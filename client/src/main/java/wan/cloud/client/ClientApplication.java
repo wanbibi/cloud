6dce505efca0c42a4e174142b2c32ebc942d4db1
wan <wan>
2018-05-01 13:51:39 +0800

    feign and ribbon

--- client/src/main/java/wan/cloud/client/ClientApplication.java
+++ client/src/main/java/wan/cloud/client/ClientApplication.java
@@ -1,12 +1,27 @@
 package wan.cloud.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Controller
 public class ClientApplication {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    @ResponseBody
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
---
+++ feign/src/main/java/wan/cloud/feign/SchedualServiceHi.java
@@ -0,0 +1,12 @@
+package wan.cloud.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "client")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
