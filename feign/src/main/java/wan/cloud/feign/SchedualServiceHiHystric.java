e1c15da3119e0e9765e115748451a966c655999d
wan <wan>
2018-05-01 14:59:24 +0800

    dashboard and zuul

---
+++ feign/src/main/java/wan/cloud/feign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package wan.cloud.feign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
